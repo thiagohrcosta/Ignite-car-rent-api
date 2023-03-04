8c2762ff10e430087fa0e182a361b64eab82257e
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-03-03 21:31:31 -0300

    adding server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import { createCourse } from './routes';
 
 const app = express();
 
 app.get("/", createCourse);
+
+app.post("/courses", (req, res) => {
+  const { name } = request.body;
+  return response.json({ name })
+})
 
 app.listen(3333)
