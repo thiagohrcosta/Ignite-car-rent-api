637196d006c998ea73709c8d1199da84c97404a5
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-03-13 16:09:20 -0300

    uploading csv file

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { CategoriesRepository } from "../modules/cars/repositories/implementations/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: "./tmp",
+});
+
 const categoriesRepository = CategoriesRepository.getInstance();
 
 categoriesRoutes.get("/", (req, res) => {
@@ -13,8 +18,14 @@ categoriesRoutes.get("/", (req, res) => {
   return res.json(all);
 });
 
-categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController.handle(request, response);
+categoriesRoutes.post("/", (req, res) => {
+  return createCategoryController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  const { file } = req;
+  console.log(file);
+  return res.send();
 });
 
 export { categoriesRoutes };
