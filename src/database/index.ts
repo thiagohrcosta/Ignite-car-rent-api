f285d0175de03370eb5f12e910316ecf9d32556b
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-03-18 23:07:44 -0300

    data base creation

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,13 @@
-// disable eslint for this file
-/* eslint-disable */
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then((options) => {
+  const newOptions = options as IOptions;
+  newOptions.host = "database";
+  createConnection({
+    ...options,
+  });
+});
